df6acebc6bd96569bcaf7c4ca21cba381347165d
zpx <zpx8179033@163.com>
2017-07-12 15:59:16 +0800

    首页基本功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 //index.js
 var lottoList=[]
-for(var i=0;i<5;i++){
+for(var i=0;i<10;i++){
   var o={};
   o.id=i;
   o.name='iphone 6';
@@ -10,6 +10,31 @@ for(var i=0;i<5;i++){
   o.image ='http://f10.baidu.com/it/u=3967905364,1965626044&fm=72';
   lottoList.push(o);
 }
+var lastLottoList = []
+for (var i = 0; i < 2; i++) {
+  var o = {};
+  o.id = i;
+  o.name = 'iphone 6';
+  o.price = 3000;
+  o.totalCopies = 3200;
+  o.currentCopies = 3199;
+  o.image = 'http://f10.baidu.com/it/u=3967905364,1965626044&fm=72';
+  lastLottoList.push(o);
+}
+
+var menus=[{
+    id:0,
+    name:'万里挑一',
+    isSelected:false
+  }, {
+    id: 1,
+    name: '一锤定音',
+    isSelected: false
+  }, {
+    id: 2,
+    name: '万里说明',
+    isSelected: false
+  }];
 
 //获取应用实例
 var app = getApp()
@@ -17,13 +42,25 @@ Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
-    lottoList:lottoList
+    lottoList:lottoList,
+    lastLottoList: lastLottoList,
+    currentIndex:0,
+    menus:menus
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs',
     })
+  },
+  toggleMenu:function(e){
+    var currentIndex = 0;
+    if (e.detail&&e.detail.current){
+      currentIndex = e.detail.current;
+    }else{
+      currentIndex=e.currentTarget.dataset.id;
+    }
+    this.setData({currentIndex:currentIndex});
   },
   toDetailPage:function(e){
     var id = e.currentTarget.dataset.id;
